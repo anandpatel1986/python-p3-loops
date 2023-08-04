ed434812b8c7ceaabc13882f0bee3ec0f3d65edf
Anandkumar Patel <avpatel2005@gmail.com>
2023-08-04 11:00:20 -0400

    complete loops lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(f"{count}")
+        if count == 1:
+            print("Happy New Year!")
+        count-=1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_list = list()
+    for number in int_list:
+        number = number * number
+        new_list.append(number)
+    return new_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i%3==0 and i%5==0:
+            print("FizzBuzz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Buzz")
+        else:
+            print(i)
